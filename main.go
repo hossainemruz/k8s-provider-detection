dd17be23f3980856208d557f035d80ca073dd308
Tamal Saha <tamal@appscode.com>
2018-10-02 06:21:43 -0700

    added-all

--- main.go
+++ main.go
@@ -5,13 +5,15 @@ import (
 	"crypto/x509"
 	"flag"
 	"fmt"
-	"k8s.io/client-go/rest"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/the-redback/go-oneliners"
-	_ "k8s.io/client-go/rest"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -26,16 +28,18 @@ func init() {
 	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
 }
 
+// kubedb-98dc2725.hcp.centralus.azmk8s.io
+
 func main() {
 	flag.Parse()
-	log.Println("kubeconfig: ",kubeConfig,"masterURL: ",masterURL)
+	log.Println("kubeconfig: ", kubeConfig, "masterURL: ", masterURL)
 	cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeConfig)
 	if err != nil {
 		log.Fatalf("can't build config. Reason: %v", err.Error())
 	}
 
-	err=rest.LoadTLSFiles(cfg)
-	if err!=nil{
+	err = rest.LoadTLSFiles(cfg)
+	if err != nil {
 		log.Println(err)
 		return
 	}
@@ -51,29 +55,48 @@ func main() {
 
 	// create ca cert pool
 	caCertPool := x509.NewCertPool()
-	ok:=caCertPool.AppendCertsFromPEM(cfg.CAData)
+	ok := caCertPool.AppendCertsFromPEM(cfg.CAData)
 
-	if !ok{
+	if !ok {
 		log.Fatalf("Can't append caCert to caCertPool.")
 	}
 
 	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{RootCAs:caCertPool},
+		TLSClientConfig: &tls.Config{RootCAs: caCertPool},
 	}
 	client := &http.Client{Transport: tr}
 
-	resp,err:=client.Get("https://kubernetes.default.svc")
-
-	if err!=nil{
+	resp, err := client.Get("https://kubernetes.default.svc")
+	if err != nil {
 		log.Println(err)
 		return
 	}
-
-	for _,cert:=range resp.TLS.PeerCertificates{
-		fmt.Println(cert.DNSNames)
+	if !resp.TLS.HandshakeComplete {
+		log.Fatalln("failed to complete TLS handshake.")
 	}
 
-	oneliners.PrettyJson(resp,"Response")
+	var addr string
+DONE:
+	for _, chain := range resp.TLS.VerifiedChains {
+		if len(chain) == 0 {
+			continue
+		}
+		for _, domain := range chain[0].DNSNames {
+			if strings.HasSuffix(domain, ".azmk8s.io") {
+				addr = domain
+				break DONE
+			}
+		}
+	}
+	fmt.Println(addr)
 
-	time.Sleep(2*time.Minute)
+	c2 := rest.CopyConfig(cfg)
+	c2.Host = addr
+
+	k2 := kubernetes.NewForConfigOrDie(c2)
+	nodes, err := k2.CoreV1().Nodes().List(metav1.ListOptions{})
+	fmt.Println(err)
+	oneliners.PrettyJson(nodes.Items)
+
+	time.Sleep(2 * time.Minute)
 }
